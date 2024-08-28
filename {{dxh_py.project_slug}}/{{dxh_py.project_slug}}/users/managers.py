c69349102d546c83f67e6bef82dffa19f8a57d7c
git-jamil <jamil.devxhub@gmail.com>
2024-08-28 14:25:36 +0600

    user name added

--- {{dxh_py.project_slug}}/{{dxh_py.project_slug}}/users/managers.py
+++ {{dxh_py.project_slug}}/{{dxh_py.project_slug}}/users/managers.py
@@ -25,7 +25,7 @@ class UserManager(DjangoUserManager["User"]):
     def create_user(self, username=None, email: str = None, password: str | None = None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
 
     def create_superuser(self, username=None, email: str = None, password: str | None = None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
@@ -36,4 +36,4 @@ class UserManager(DjangoUserManager["User"]):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, email, password, **extra_fields)
