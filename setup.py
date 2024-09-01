969b95488bdebcc101a0b73436976122c29b3802
git-jamil <jamil.devxhub@gmail.com>
2024-09-01 21:30:26 +0600

    Update repo link

--- setup.py
+++ setup.py
@@ -5,32 +5,32 @@ except ImportError:
     from distutils.core import setup
 
 # We use calendar versioning
-version = "2023.04.28"
+version = "2024.08.26"
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name="dxh_py-django",
+    name="django-boilerplate",
     version=version,
     description=("A dxh_py template for creating production-ready " "Django projects quickly."),
     long_description=long_description,
     author="DEVxHUB",
     author_email="tech@devxhub.com",
-    url="https://github.com/dxh_py/dxh_py-django",
+    url="https://github.com/devxhub/django-boilerplate",
     packages=[],
     license="MIT",
     zip_safe=False,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Console",
-        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 5.0",
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development",
     ],
